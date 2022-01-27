c447a28527132c7aa6f08cedd7d875b95cc01045
himanshuhsn <himanshu000666@gmail.com>
2022-01-27 18:18:50 +0530

    add: stubs for core logic

--- src/shorturl/core/urls.py
+++ src/shorturl/core/urls.py
@@ -1,8 +1,13 @@
 def create_url(api_key, body):
+    # validate the api_key
+    # add the url to the shorturl table
     pass
 
 def delete_url(api_key, shorturl):
+    # validate the api_key
+    # delete the shorturl
     pass
 
 def redirect_url(shorturl):
+    # get the long url from short url
     pass
--- src/shorturl/core/users.py
+++ src/shorturl/core/users.py
@@ -1,11 +1,24 @@
+from .. import config
+
 def create_user(body):
+    # get the api_key for this user
+    # get the quota from config.py
+    # insert item into user table
     pass
 
 def get_key(username, password):
+    # check username and password
+    # get the item from user table using username
     pass
 
-def update_key(username, password, body):
+def update_key(username, password):
+    # check username and password
+    # get a new key
+    # update the key to user table
     pass
 
 def update_user(username, body):
+    # check username and password
+    # get the hashed of new password
+    # update the user table with the new password
     pass
